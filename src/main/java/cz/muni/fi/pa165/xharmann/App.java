e666d0f23ab3cb81343e4c834b0ae7aff8d6eb18
Peter Harmann <xharmann@nymfe63.fi.muni.cz>
2018-09-18 13:38:39 +0200

    Initial commit.

---
+++ src/main/java/cz/muni/fi/pa165/xharmann/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.xharmann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
